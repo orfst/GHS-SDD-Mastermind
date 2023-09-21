712b127070b1e1be6faa7dbc7893588c29b91c5a
Adam/AJ <adam.jackson31@education.nsw.gov.au>
2023-09-21 14:43:54 +1000

    Updated main.py
    Made a final update to main.py to add comments to each section of the code, and adjusted the loss message to be more friendly

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+#Importing libraries
 import random as codeMaker
 import sys as killSwitch
 
 #Generate 4 unique numbers between 1 and 9
 numbers = codeMaker.sample(range(1, 10), 4)
 
-print(numbers)
-
+#Variables
 correct1 = 0
 correct2 = 0
 correct3 = 0
@@ -24,8 +24,9 @@ answer1 = numbers[findAnswer1]
 answer2 = numbers[findAnswer2]
 answer3 = numbers[findAnswer3]
 answer4 = numbers[findAnswer4]
-print(answer1, answer2, answer3, answer4)
 
+#Defining required functions for the game
+#This is for the Codebreaker to make their guess, or for the Codebreaker to ask for help or quit the game
 def getGuess():
     global guess1, guess2, guess3, guess4, guess
     print("Welcome to Mastermind!")
@@ -51,7 +52,9 @@ def getGuess():
         
     print(guess1, guess2, guess3, guess4)
 
-
+#This function checks each of the numbers the Codebreaker has guessed,
+#giving them a value related to whether the number is
+#correct, in the wrong spot or not in the code
 def checkGuess():
     global correct1, correct2, correct3, correct4
     if guess1 == answer1:
@@ -82,37 +85,39 @@ def checkGuess():
     else:
         correct4 = 3
     
-
+#This function provides the feedback for the Codebreaker's guess,
+#based on the value assigned to each number in the function before
 def guessResults():
     if correct1 == 1:
         print("The first number you guessed is correct.")
     elif correct1 == 2:
-        print("The first number you guessed is in the wrong spot.")
+        print("The first number you guessed is in the code, but is also in the wrong spot.")
     else:
         print("The first number you guessed is not in the code.")
 
     if correct2 == 1:
         print("The second number you guessed is correct.")
     elif correct2 == 2:
-        print("The second number you guessed is in the wrong spot.")
+        print("The second number you guessed is in the code, but is also in the wrong spot.")
     else:
         print("The second number you guessed is not in the code.")
 
     if correct3 == 1:
         print("The third number you guessed is correct.")
     elif correct3 == 2:
-        print("The third number you guessed is in the wrong spot.")
+        print("The third number you guessed is in the code, but is also in the wrong spot.")
     else:
         print("The third number you guessed is not in the code.")
 
     if correct4 == 1:
         print("The fourth number you guessed is correct.")
     elif correct4 == 2:
-        print("The fourth number you guessed is in the wrong spot.")
+        print("The fourth number you guessed is in the code, but is also in the wrong spot.")
     else:
         print("The fourth number you guessed is not in the code.")
 
-while guessCount < 3:
+#This is the main loop, using the three functions from before
+while guessCount < 8:
     getGuess()
     checkGuess()
     guessResults()
@@ -124,4 +129,4 @@ while guessCount < 3:
 if correct1 == correct2 == correct3 == correct4 == 1:
     print("Correct! You win!")
 else:
-    print("You didn't win. Try again!")
+    print("You didn't win. Better luck next time, you'll get it eventually. :)")
